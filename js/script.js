82f458e880c389fb2648bba40382eb31ff32005e
Hamaddogar <hamadali77115033@gmail.com>
2020-04-10 20:54:34 +0500

    1st assigmnet

---
+++ js/script.js
@@ -0,0 +1,14 @@
+   function  submitform(event)
+   {
+        event.preventDefault()
+       var username =  document.getElementById("uname").value;
+       var userEmail =  document.getElementById("userEmail").value;
+        
+         var password=document.getElementById("uPassword").value
+        console.log("usernanmem",username,userEmail,password)
+
+         
+
+   }
+
+
